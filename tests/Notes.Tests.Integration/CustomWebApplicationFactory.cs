a6aaacfdf31fcebb2e79b99c51ded68b1bfc96cc
omio-ma <84961314+omio-ma@users.noreply.github.com>
2025-06-19 22:18:20 +0100

    making config file optional

--- tests/Notes.Tests.Integration/CustomWebApplicationFactory.cs
+++ tests/Notes.Tests.Integration/CustomWebApplicationFactory.cs
@@ -11,7 +11,7 @@ public class CustomWebApplicationFactory : WebApplicationFactory<API.Program>
         builder.ConfigureAppConfiguration((context, config) =>
         {
             var path = Path.Combine(AppContext.BaseDirectory, "appsettings.Test.json");
-            config.AddJsonFile(path, optional: false);
+            config.AddJsonFile(path, optional: true);
         });
     }
 }
