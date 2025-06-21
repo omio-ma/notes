5a4d22069c340db3d75d9c5b3e6d5c58b8780cdb
omio-ma <84961314+omio-ma@users.noreply.github.com>
2025-06-21 11:54:30 +0100

    created new react app for ui

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 min-h-screen flex items-center justify-center">
+      <Button variant="contained">Hello notes</Button>
+    </div>
+  );
+}
+
+export default App;
