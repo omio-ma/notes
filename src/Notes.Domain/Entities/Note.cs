95a835185eece3c3c4c87c0105e8259424296733
omio-ma <84961314+omio-ma@users.noreply.github.com>
2025-06-28 14:30:14 +0100

    updated note domain model

--- src/Notes.Domain/Entities/Note.cs
+++ src/Notes.Domain/Entities/Note.cs
@@ -5,5 +5,5 @@ public class Note
     public int Id { get; set; }
     public string Title { get; set; } = default!;
     public string Content { get; set; } = default!;
-    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
